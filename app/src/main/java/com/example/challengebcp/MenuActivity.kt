031abb584c2f068fcf3192fe68d26e705f442696
Josué Durand Pebe <josue.durand8@gmail.com>
2022-01-15 19:07:59 -0500

    second part of menu

--- app/src/main/java/com/example/challengebcp/MenuActivity.kt
+++ app/src/main/java/com/example/challengebcp/MenuActivity.kt
@@ -4,39 +4,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MenuActivity : AppCompatActivity() {
-    private lateinit var tiSendTextInputLayout: TextInputLayout
+
     private lateinit var sendEditText: TextInputEditText
-    override fun onStart() {
-        val normalColorStateList = ContextCompat.getColorStateList(this, R.color.your_custom_grey)
-        tiSendTextInputLayout.setStartIconTintList(normalColorStateList)
-        super.onStart()
-    }
+    private lateinit var getEditText: TextInputEditText
+    //TextView
+    private lateinit var purchaseTextView: TextView
+    private lateinit var saleTextView: TextView
+    private lateinit var firstCoinTextView:TextView
+    private lateinit var secondCoinTextView:TextView
+
+
+    private lateinit var changeImageView:ImageView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        tiSendTextInputLayout = findViewById(R.id.til_firstTextInputLayout)
-        sendEditText = findViewById(R.id.sendEditText)
 
-        sendEditText.setOnFocusChangeListener { v, hasFocus ->
-            when {
-                !hasFocus -> {
-                    val normalColorStateList = ContextCompat.getColorStateList(applicationContext, R.color.your_custom_grey)
-                    tiSendTextInputLayout.setStartIconTintList(normalColorStateList)
-                }
-                else -> {
-                    val normalColorStateList =
-                        ContextCompat.getColorStateList(applicationContext, R.color.blue)
-                    tiSendTextInputLayout.setStartIconTintList(normalColorStateList)
-                }
+        sendEditText = findViewById(R.id.sendEditText)
+        getEditText = findViewById(R.id.getEditText)
+
+        changeImageView = findViewById(R.id.changeImageView)
+        firstCoinTextView = findViewById(R.id.firstCoinTextView)
+        secondCoinTextView = findViewById(R.id.secondCoinTextView)
+
+        changeImageView.setOnClickListener {
+            sendEditText.setText("")
+            getEditText.setText("")
+            val firstCoinValue = firstCoinTextView.text.toString()
+            val secondCoinValue = secondCoinTextView.text.toString()
+            if (firstCoinValue == "Dólares"){
+                firstCoinTextView.text = secondCoinValue
+                secondCoinTextView.text = firstCoinValue
+            }else if (firstCoinValue == "Soles"){
+                firstCoinTextView.text = secondCoinValue
+                secondCoinTextView.text = firstCoinValue
             }
         }
     }
+
+
 
 
 }
